29ca3051073e0f80ecc7eac61261ab3d114fed14
pyounani <yndbsk9372@gmail.com>
2025-02-23 10:53:53 +0900

    [Fix] 읽기 전용 트랜잭션 적용 #57

--- src/main/java/com/example/starhub/service/TechStackService.java
+++ src/main/java/com/example/starhub/service/TechStackService.java
@@ -13,7 +13,7 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
+@Transactional(readOnly = true)
 public class TechStackService {
 
     private final TechStackRepository techStackRepository;
