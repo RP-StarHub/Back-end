d76c545220e9899d78d8943d35e3670077387660
pyounani <yndbsk9372@gmail.com>
2025-04-07 17:20:27 +0900

    [!HOTFIX] 기본 생성자 누락 -> 추가

--- src/main/java/com/example/starhub/dto/request/UsernameCheckRequestDto.java
+++ src/main/java/com/example/starhub/dto/request/UsernameCheckRequestDto.java
@@ -2,10 +2,12 @@ package com.example.starhub.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class UsernameCheckRequestDto {
 
